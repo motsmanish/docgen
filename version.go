fbc7a2dd73da3c4c61f1cd10ed8ee616896487f1
Manish Motwani <motsmanish@users.noreply.github.com>
2019-02-18 18:08:12 +0530

    Update version.go

--- version.go
+++ version.go
@@ -12,11 +12,7 @@ var versionCmd = &cobra.Command{
 	Long:  `Provide version information`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println(logo)
-<<<<<<< HEAD
 		fmt.Println("Docgen version: v2.1")
-=======
-		fmt.Println("Docgen version: v2.1 - testing edit")
->>>>>>> 258ed8768a14ed29f6b6d6617125d5691d4d74ca
 		fmt.Println("Support postman collection version > 2.1")
 	},
 }
